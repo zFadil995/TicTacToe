7d7c71e207eb761b06a699adbfff3303c8fdb519
Fadil Žilić <zfadil995@gmail.com>
2017-04-26 14:54:07 +0200

    Layout Complete.

--- TicTacToe/TicTacToe/MainPage.xaml.cs
+++ TicTacToe/TicTacToe/MainPage.xaml.cs
@@ -9,9 +9,42 @@ namespace TicTacToe
 {
     public partial class MainPage : ContentPage
     {
+        String currentPlayer = "O";
         public MainPage()
         {
             InitializeComponent();
+            CurrentPlayer.Text = "Playing: " + currentPlayer;
+        }
+
+        private void NewGameClicked(object sender, EventArgs e)
+        {
+            TL.Text = "";
+            CL.Text = "";
+            BL.Text = "";
+            TR.Text = "";
+            CR.Text = "";
+            CR.Text = "";
+            TC.Text = "";
+            CC.Text = "";
+            CR.Text = "";
+        }
+
+        private void PlayClicked()
+        {
+            if (currentPlayer == "O")
+                currentPlayer = "X";
+            else if (currentPlayer == "X")
+                currentPlayer = "O";
+            CurrentPlayer.Text = "Playing: " + currentPlayer;
+        }
+
+        private void PlayMoveClicked(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text != "X" && ((Button)sender).Text != "O")
+            {
+                ((Button)sender).Text = currentPlayer;
+                PlayClicked();
+            }
         }
     }
 }
